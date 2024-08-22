152738fe29a8c5cebbd00c17a051fbd528aad4f1
mkpage1337 <mrku1337@gmail.com>
2024-08-22 17:49:04 +0200

    downloading elements from the page

---
+++ script.js
@@ -0,0 +1,10 @@
+let timer;
+let isRunning = false;
+let minutes = 25;
+let seconds = 0;
+
+const minutesTimer = document.getElementById("minutes");
+const secondsTimer = document.getElementById("seconds");
+const startBtn = document.getElementById("start-btn");
+const pauseBtn = document.getElementById("pause-btn");
+const resetBtn = document.getElementById("reset-btn");
